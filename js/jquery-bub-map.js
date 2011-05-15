697897342470ba8a909abd664b82dfc40d0c09db
jsonvi <jsonvi@gmail.com>
2011-05-15 17:51:22 +0800

    core bubmap js file added

---
+++ js/jquery-bub-map.js
@@ -0,0 +1,8 @@
+/*
+ * Bub Map v1.1.0 - jQuery plugin for highly customizable Google Maps
+ *
+ * Copyright (c) 2011 Jason Guo (jsonvi.com)
+ * MIT License: http://www.opensource.org/licenses/mit-license.php
+ *
+ */
+
